3c7155f006455dde2360e6352eb2f3dddd67b0b4
Kohey Aoyama <kou_713@hotmai.com>
2021-03-20 23:57:51 +0900

    fix(card.js): fix corner radius

--- src/components/card.js
+++ src/components/card.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Tag from "../components/tag"
 import { StaticImage } from "gatsby-plugin-image"
 import VariableImage from "../utils/variable-image"
+import thumbImg from "../images/thumb-sample.jpeg"
 
 const Card = () => {
   return (
     <div className="w-full h-full bg-white shadow-md pb-1 rounded-xl">
       <VariableImage>
-      <StaticImage className="w-full h-full rounded-t-xl" src="../images/thumb-sample.jpeg" />
+        {//<StaticImage className="w-full h-full rounded-t-xl" src="../images/thumb-sample.jpeg" />
+}
+        <img className="w-full h-full rounded-t-xl object-cover" src={thumbImg} />
       </VariableImage>
       <div className="p-3">
       <p className="text-lg font-bold pb-2">ワークタイトル</p>
