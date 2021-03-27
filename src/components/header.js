019a814b2095890a43c50932dfe3592d397d796e
Kohey Aoyama <kou_713@hotmai.com>
2021-03-27 12:58:32 +0900

    feat(header.js): modify style

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,10 @@ import { urlFacebook, urlGitHub } from "../utils/constants"
 
 const HeaderMobile = () => {
   return (
-    <div className="w-screen h-24 px-4 pb-4 fixed z-50 bg-white bg-opacity-70 flex items-center">
+    <div className="
+      w-screen h-24 px-4 pb-4 fixed z-50 bg-white bg-opacity-70 flex items-center
+
+    ">
       <div className="h-full flex items-end flex-grow">
         <h1 className="text-4xl font-extrabold antialiased">Koheybase</h1>
 
