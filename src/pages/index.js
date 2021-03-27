e7204a74680f53766c398646cd92c7fb378949de
Kohey Aoyama <kou_713@hotmai.com>
2021-03-27 11:36:07 +0900

    feat(index.js): update layout according to card style updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => {
   return (
     <ScaffoldMobile>
       <div className="w-screen flex flex-col space-y-4">
-        <div className="mb-2 px-4">
+        <div className="mb-4 px-4">
           <p>青山広平</p>
           <p>東京を拠点とするデジタルプロダクトデザイナー</p>
         </div>
