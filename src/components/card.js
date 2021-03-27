3e46850919797755a4407f9169460a22a9239b82
Kohey Aoyama <kou_713@hotmai.com>
2021-03-27 20:55:34 +0900

    feat(contentful api): connect contentful api and show it on home page

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Tag from "../components/tag"
 
-const Card = () => {
-
+const Card = ({ data }) => {
+  const thumbnail = getImage(data.thumbnail)
   return (
     <div className="w-full h-full bg-white pb-10">
-      <StaticImage
+      <GatsbyImage
         alt="Thumbnail"
-        aspectRatio={2/1}
-        //imgStyle={{borderTopLeftRadius: '0.75rem',borderTopRightRadius: '0.75rem'}}
-        src="../images/thumb-sample.jpeg"
+        image={thumbnail}
+        className="hover:shadow-lg"
       />
       <div className="px-3">
-        <p className="text-lg font-bold">ワークタイトル</p>
-        <Tag>UI/UXデザイン</Tag>
-        <Tag>UI/UXデザイン</Tag>
+        <p className="text-lg font-bold">{data.title}</p>
+        {data.genre.map((genre, index) => (
+          <Tag key={index}>{genre}</Tag>
+        ))}
       </div>
     </div>
   )
