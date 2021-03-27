39f97dd662f3033dd127411285dccba25fd8ab05
Kohey Aoyama <kou_713@hotmai.com>
2021-03-27 12:00:08 +0900

    feat(index.js): add grid layout to works list

--- src/components/card.js
+++ src/components/card.js
@@ -5,14 +5,14 @@ import Tag from "../components/tag"
 const Card = () => {
 
   return (
-    <div className="w-full h-full bg-white pb-6">
+    <div className="w-full h-full bg-white pb-10">
       <StaticImage
         alt="Thumbnail"
         aspectRatio={2/1}
         //imgStyle={{borderTopLeftRadius: '0.75rem',borderTopRightRadius: '0.75rem'}}
         src="../images/thumb-sample.jpeg"
       />
-      <div className="px-3 ">
+      <div className="px-3">
         <p className="text-lg font-bold">ワークタイトル</p>
         <Tag>UI/UXデザイン</Tag>
         <Tag>UI/UXデザイン</Tag>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,23 @@ import Card from "../components/card"
 const IndexPage = () => {
   return (
     <ScaffoldMobile>
-      <div className="w-screen flex flex-col space-y-4">
-        <div className="mb-4 px-4">
+      <div className="w-screen flex flex-col">
+        <div className="mb-12 px-4">
           <p>青山広平</p>
           <p>東京を拠点とするデジタルプロダクトデザイナー</p>
         </div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+
+        <div className="
+          md:grid md:grid-cols-3 md:grid-flow-row md:gap-4 md:px-4
+          ">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+
+        </div>
+
       </div>
     </ScaffoldMobile>
   )
