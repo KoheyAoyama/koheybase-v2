540b5fb083863b8bbe3f4f1252c7d009628bb46b
Kohey Aoyama <kou_713@hotmai.com>
2021-03-28 18:06:41 +0900

    feat(about-me.js): modify section title font style

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -21,14 +21,14 @@ const AboutMe = () => {
 
         <div className="px-4">
           <div className="mb-12">
-            <h2 className="text-2xl font-bold">青山広平</h2>
+            <h2 className="text-lg font-bold">青山広平</h2>
             <p className="text-sm font-light text-gray-400 mb-4">Kohey Aoyama</p>
             <p className="mb-4">千葉県在住、都内ベンチャー企業に所属のUIデザイナー。</p>
             <p className="mb-4">サービスに特化した、効果的で拡張性の高いUIキットを最速で構築。開発スピードが優先されるフェーズにおいてもクオリティを疎かにせず、双方を両立したUIデザインを提供します。これにより、特に新規事業でMVPを必要とするようなフェーズで強力にサポートできます。</p>
           </div>
 
           <div className="mb-12">
-            <h2 className="text-2xl font-bold mb-4">スキル</h2>
+            <h2 className="text-lg font-bold mb-4">スキル</h2>
             <div className="flex flex-wrap">
               <div className="mr-2 mb-1"><Tag>UIデザイン</Tag></div>
               <div className="mr-2 mb-1"><Tag>UXデザイン</Tag></div>
@@ -42,7 +42,7 @@ const AboutMe = () => {
           </div>
 
           <div>
-            <h2 className="text-2xl font-bold">略歴</h2>
+            <h2 className="text-lg font-bold">略歴</h2>
             <div>
               <h3 className="h-16 flex items-center">Nanameue.inc</h3>
               <div className="flex items-center">
