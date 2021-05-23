a5cd0dd05e4d62ee16aab037f9653be8fb2f9421
Kohey Aoyama <kou_713@hotmai.com>
2021-05-23 21:01:58 +0900

    feat(about-me.js): update content and layout

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -19,20 +19,39 @@ const AboutMe = () => {
         />
 
         <div className="px-4">
-          <div className="mb-12">
-            <h2 className="text-lg font-bold">青山広平</h2>
-            <p className="text-sm font-light text-gray-600 mb-4 ml-0.5">Kohey Aoyama</p>
-            <p className="mb-4">千葉県在住、都内ベンチャー企業に所属のUIデザイナー。</p>
-            <p className="mb-4">サービスに特化した、効果的で拡張性の高いUIキットを最速で構築。開発スピードが優先されるフェーズにおいてもクオリティを疎かにせず、双方を両立したUIデザインを提供します。これにより、特に新規事業でMVPを必要とするようなフェーズで強力にサポートできます。</p>
+          <div className="mb-16">
+            <h2 className="text-lg font-bold">
+              青山広平
+            </h2>
+            <p className="text-sm font-light text-gray-600 mb-4 ml-0.5">
+              Kohey Aoyama
+            </p>
+            <p className="mb-4">
+              千葉県在住、都内ベンチャー企業に所属のUIデザイナー。
+            </p>
+            <p className="mb-4">
+              工業高専卒業後、機械系メーカー2社で開発および設計職に就いた後にUIデザイナーに転身。これまで10個以上のプロダクト運営に携わり、最大ユーザー規模は登録300万、DAU10万。
+            </p>
+            <p className="mb-4">
+              『広い視野で考え、徹底的に作り込む。そのために一番のユーザであれ』を理念に『コアバリューを確実にユーザーに突き刺すデザイン』を実践。
+            </p>
+            <p className="mb-4">
+              日英対応、猫派、バイク乗り。
+            </p>
           </div>
 
           <div>
-            <h2 className="text-lg font-bold">略歴</h2>
+            <h2 className="text-lg font-bold mb-4">略歴</h2>
             <div>
-              <h3 className="h-16 flex items-center">Nanameue.inc</h3>
-              <div className="flex items-center">
-                <p className="flex-grow">UIデザイナー</p>
-                <p>2019-現在</p>
+              <div className="md:flex md:items-center">
+                <h3 className="
+
+                  md:h-16 md:flex md:items-center md:flex-grow"
+                >
+                  株式会社ナナメウエ
+                </h3>
+                <p className="mr-4">リードデザイナー</p>
+                <p className="text-gray-400">2019-現在</p>
               </div>
             </div>
           </div>
